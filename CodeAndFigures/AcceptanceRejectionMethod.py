b2dbf5320766ce62f2cc30524661e90ad490c323
Sandy <sandy.girl1788@gmail.com>
2019-11-25 23:19:13 -0500

    Finish AcceptanceRejectionMethod problem. Add code and figures and table.

--- CodeAndFigures/AcceptanceRejectionMethod.py
+++ CodeAndFigures/AcceptanceRejectionMethod.py
@@ -10,26 +10,147 @@ Acceptance/Rejection Method
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import SetupPlots as SP
+import scipy.interpolate as interpolate
+import pandas as pd
 #%% Definitions
 
 def probx(x):
   return np.sin(x)/2
 
 def func1(x):
-  return .6*np.ones(len(x))
+  x=np.array(x)
+  return .6*np.ones(x.size)
+
+def Area1(x):
+  return .6*x
 
 def func2(x):
-  return (-x**2+np.pi*x)/3
+  return -.2*x**2+.2*np.pi*x+.05
 
+def Area2(x):
+  return -(.2/3)*x**3 + .1*np.pi*x**2 + .05*x
 
+#%% 
 
 x=np.linspace(0,np.pi,100)
-
+#desired probability distribution
 px=np.sin(x)/2
+#analytic and integrable function f(x):constant
+fx1=func1(x)
+#analytic and integrable function f(x):quadratic
+fx2=func2(x)
 
-plt.plot(x,px,label='px')
-plt.plot(x,func1(x),label='f1')
-plt.plot(x,func2(x),label='f2')
-plt.legend()
+#%% Accept-Reject Algorithm Constant Function
+N=10000
+x0Array=np.zeros(N)
+f1Array=np.zeros(N)
 
+#determine the total Area under the curve
+A=Area1(np.pi)
+#inverse function Fx=int(f(x))
+Fxinv=interpolate.interp1d(Area1(x),x)
+
+for i in range(N):
+  # Draw a uniform deviate from [0,A]
+  y=A*np.random.uniform()  
+  #Evaluate the inverse function 
+  x0=Fxinv(y)  
+  #2nd deviate from [0,f(x0)]
+  f1=func1(x0)*np.random.uniform()
+  
+  if f1<=probx(x0):
+    #accept f1
+    x0Array[i]=x0
+    f1Array[i]=f1
+
+infactor1=N/len(x0Array[x0Array!=0])
+print('Inefficiency Factor Constant: %1.2f'\
+      %infactor1)
+print('Area Constant: %1.2f'%A)
+
+#%% Accept-Reject Algorithm Constant Function
+
+#Initialize Arrays
+x0Array2=np.zeros(N)
+f1Array2=np.zeros(N)
+
+#determine the total Area under the curve
+A2=Area2(np.pi)
+#inverse function Fx=int(f(x))
+Fxinv2=interpolate.interp1d(Area2(x),x)
+
+for i in range(N):
+  # Draw a uniform deviate from [0,A]
+  y=A2*np.random.uniform()  
+  #Evaluate the inverse function 
+  x0=Fxinv2(y)  
+  #2nd deviate from [0,f(x0)]
+  f2=func2(x0)*np.random.uniform()
+  
+  if f2<=probx(x0):
+    #accept f1
+    x0Array2[i]=x0
+    f1Array2[i]=f2
+
+infactor2=N/len(x0Array2[x0Array2!=0])
+print('Inefficiency Factor Quadratic:%1.2f'\
+      %infactor2)
+print('Area Quadratic %1.2f'%A2)
+
+#%% Plot constant function
+width,height=SP.setupPlot(singleColumn=False)
+
+fig,axs = plt.subplots(1,1,
+                       figsize=(width,height),
+                       sharex=True,)
+
+axs.plot(x,px,label='px')
+axs.plot(x,fx1,label='f1')
+axs.scatter(x0Array[x0Array!=0],
+   f1Array[f1Array!=0],
+   label='probx',color='C0',alpha=0.5)
+axs.legend()
+axs.grid()
+axs.set_xlim([-.1,np.pi+.1])
+axs.set_ylim([-.05,.65])
+
+fig.savefig('AcceptanceRejectionPlotf1.pdf')
+
+#%% Plot quadratic function
+width,height=SP.setupPlot(singleColumn=False)
+
+fig,axs = plt.subplots(1,1,
+                       figsize=(width,height),
+                       sharex=True,)
+
+axs.plot(x,px,label='px')
+axs.plot(x,fx2,label='f2')
+axs.scatter(x0Array2[x0Array2!=0],
+   f1Array2[f1Array2!=0],
+   label='probx2',color='C0',alpha=0.5)
+axs.legend()
+axs.grid()
+axs.set_xlim([-.1,np.pi+.1])
+axs.set_ylim([-.05,.65])
+
+fig.savefig('AcceptanceRejectionPlotf2.pdf')
+
+#%% Save Data to csv file
+
+indexNames=['Constant','Quadratic']
+colNames=np.array(['$Ineficiency Factor$',
+                   '$TotalArea$'])
+row1=np.array([infactor1,A])
+row2=np.array([infactor2,A2])
+rows=[row1,row2]
+
+df = pd.DataFrame(rows,columns=colNames,
+                  index=indexNames)
+
+with open('AcceptanceRejectionTable.tex',
+          'w') as tf:
+    tf.write(df.to_latex(float_format='%2.4f',
+                         index=True,
+                         escape=False))
+    
